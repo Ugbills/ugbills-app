64edc6e7974f4fce0340572df4546139c0a61a4a
Justice Philemon <imailasjay>
2024-07-27 18:55:28 +0100

    ...

---
+++ lib/screens/user/more/refer-and-earn/earnings.dart
@@ -0,0 +1,164 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:zeelpay/screens/user/more/refer-and-earn/history.dart';
+import 'package:zeelpay/screens/widgets/texts_widget.dart';
+import 'package:zeelpay/themes/palette.dart';
+
+class ReferralEarnings extends StatelessWidget {
+  ReferralEarnings({super.key});
+app
+  final List referrals = [
+    ["Henry Eze", "06:59 PM", "18 Mar", false],
+    ["Akin Layi", "06:59 PM", "18 Mar", false],
+    ["Ogonna Iho", "06:59 PM", "18 Mar", true],
+    ["Michael Ade", "06:59 PM", "18 Mar", false],
+    ["Zainab Zee", "06:59 PM", "18 Mar", true],
+    ["Abimbola Love", "06:59 PM", "18 Mar", true],
+    ["Oyin Lawal", "06:59 PM", "18 Mar", false],
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: ListView(
+          padding: const EdgeInsets.symmetric(horizontal: 24).copyWith(top: 24),
+          children: [
+            showBalance(),
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 12),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  const ZeelTextFieldTitle(text: "Referral Status"),
+                  GestureDetector(
+                    onTap: () {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (_) => const ReferralHistory(),
+                          ));
+                    },
+                    child: Row(
+                      children: [
+                        const ZeelTextFieldTitle(text: "View all"),
+                        const SizedBox(width: 6),
+                        Container(
+                          padding: const EdgeInsets.all(4),
+                          decoration: const BoxDecoration(
+                            shape: BoxShape.circle,
+                            color: ZealPalette.primaryPurple,
+                          ),
+                          child: const Icon(
+                            Icons.arrow_forward_ios_rounded,
+                            size: 16,
+                            color: Colors.white,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            if (referrals.isEmpty)
+              const Center(
+                child: Text("No Referral Yet!"),
+              )
+            else
+              ListView.builder(
+                itemCount: referrals.length,
+                shrinkWrap: true,
+                physics: const NeverScrollableScrollPhysics(),
+                itemBuilder: (context, index) {
+                  return referralStatus(
+                    referrals[index][3],
+                    referrals[index][0],
+                  );
+                },
+              )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+Widget showBalance() {
+  return Container(
+    padding: const EdgeInsets.all(24),
+    alignment: Alignment.center,
+    decoration: BoxDecoration(
+      color: ZealPalette.primaryPurple,
+      borderRadius: BorderRadius.circular(24),
+    ),
+    child: Column(
+      children: [
+        const Text(
+          "Total Earnings",
+          style: TextStyle(color: Colors.white),
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text(
+              "₦73,000.00",
+              style: TextStyle(
+                color: Colors.white,
+                fontWeight: FontWeight.w700,
+                fontSize: 22,
+              ),
+            ),
+            IconButton(
+              onPressed: () {},
+              icon: const Icon(
+                Icons.remove_red_eye_outlined,
+                color: Colors.white,
+              ),
+            ),
+          ],
+        ),
+      ],
+    ),
+  );
+}
+
+Widget referralStatus(bool isPending, String name) {
+  return Container(
+    margin: const EdgeInsets.symmetric(vertical: 6),
+    padding: const EdgeInsets.all(16),
+    decoration: BoxDecoration(
+      color: Colors.white,
+      borderRadius: BorderRadius.circular(16),
+    ),
+    child: Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            ZeelTextFieldTitle(text: name),
+            const Text(
+              "06:59 PM • 18 Mar",
+              style: TextStyle(color: Colors.grey),
+            ),
+          ],
+        ),
+        Container(
+          padding: const EdgeInsets.symmetric(horizontal: 8),
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(50),
+            color: isPending ? ZealPalette.peach : ZealPalette.lightGreen,
+          ),
+          child: Text(
+            isPending ? "Pending" : "Completed",
+            style: TextStyle(
+              color:
+                  isPending ? ZealPalette.rustColor : ZealPalette.successGreen,
+            ),
+          ),
+        ),
+      ],
+    ),
+  );
+}
